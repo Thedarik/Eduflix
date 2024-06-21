bd815174dc5f250e2e06dfc293de9b9ac6d05dd2
The Darik <thedarik2021@gmail.com>
2024-06-21 18:06:23 +0500

    Samandar birinchi commit

---
+++ lib/src/core/style/colors.dart
@@ -0,0 +1,58 @@
+import 'dart:ui';
+
+sealed class AppColors {
+  static bool hh = false;
+
+  /// main color
+  static const transparent = Color(0x00000000);
+  static const white = Color(0xffffffff);
+  static const black = Color(0xFF000000);
+  static const red = Color(0xFFFF0000);
+  static const green = Color(0xFF03c04a);
+
+  /// every color
+  static const cFADF6F = Color(0xFFFADF6F);
+  static const c343A45 = Color(0xFF343A45);
+  static const c7A7E89 = Color(0xFF7A7E89);
+  static const c4ECB71 = Color(0xFF4ECB71);
+  static const cA3F74E = Color(0xFFA3F74E);
+  static const c9DE892 = Color(0xFF9DE892);
+  static const c1A1A1A = Color(0xFF1A1A1A);
+  static const cD9D9D9 = Color(0xFFD9D9D9);
+  static const c767676 = Color(0xFF767676);
+  static const c1A1C1E = Color(0xFF1A1C1E);
+  static const c1D1D1D = Color(0xFF1D1D1D);
+  static const cC7C7C7 = Color(0xFFC7C7C7);
+  static const c666666 = Color(0xFF666666);
+  static const c1A431A = Color(0xFF1A431A);
+  static const c545454 = Color(0xFF545454);
+  static const cE0E0E0 = Color(0xFFE0E0E0);
+  static const c22FF01 = Color(0xFF22FF01);
+  static const c65C737 = Color(0xFF65C737);
+  static const c262626 = Color(0xFF262626);
+  static const c717171 = Color(0xFF717171);
+  static const cE51837 = Color(0xFFE51837);
+  static const cFFF2F3 = Color(0xFFFFF2F3);
+  static const cE4E4E4 = Color(0xFFE4E4E4);
+  static const c1B406A = Color(0xFF1B406A);
+  static const c7E4AF5 = Color(0xFF7E4AF5);
+  static const cF64B06 = Color(0xFFF64B06);
+  static const c507844 = Color(0xFF507844);
+  static const c211F1F = Color(0xFF211F1F);
+  static const c979797 = Color(0xFF979797);
+  static const cEEEEEE = Color(0xFFEEEEEE);
+  static const c6F1671 = Color(0xFF6F1671);
+  static const cFBBC09 = Color(0xFFFBBC09);
+  static const c258D00 = Color(0xFF258D00);
+  static const c64DA40 = Color(0xFF64DA40);
+  static const c181818 = Color(0xFF181818);
+  static const c808080 = Color(0xFF808080);
+  static const c97F69B = Color(0xFF97F69B);
+
+  /// The master color
+  static const cFF0000 = Color(0xFFFF0000);
+  static const cFF7A00 = Color(0xFFFF7A00);
+  static const cFFF500 = Color(0xFFFFF500);
+  static const c42FF00 = Color(0xFF42FF00);
+  static const c0057FF = Color(0xFF0057FF);
+}
